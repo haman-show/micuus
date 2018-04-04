f3da00047c710be90fbc3e391796fdecee53fcf8
Terashima <yamanogomora@gmail.com>
2018-04-04 15:14:40 +0900

    デザインを当てはめる準備

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
 <?php get_header(); ?>
+<section class="top">
+  <div class="container">
+    <div class="title">
+      <h1>SECRET GIRLS GARDEN</h1>
+      <p>Product by ASUMI ENDO</p>
+    </div>
+  </div>
+</section>
+<section class="welcome">
+  <div class="container">
+    <div class="component title">
+      <h1>Welcome</h1>
+      <hr>
+      <p>SECRET GIRLS GARDEN へお越し頂きまして有難うございます。 新着作品や、イベントなど各種サービスのご案内を随時更新しております。</p>
+    </div>
+  </div>
+</section>
 <?php get_footer(); ?>
+
+
