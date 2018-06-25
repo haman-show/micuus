603a14ebcd63dfec798e4a9671b22a4f8d929c36
Terashima <yamanogomora@gmail.com>
2018-06-25 14:32:12 +0900

    headerとwelcome作成

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge7">
   <meta name="viewport" content="width=device-width initial-scale=1">
   <title><?php bloginfo('name'); ?></title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css" />
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/flexslider.css" />
   <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" />
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php get_header(); ?>
+<header>
+  <h1><img src="<?php echo get_template_directory_uri(); ?>/images/txt_title.png" width="630" height="65" /></h1>
+  <nav>
+    <ul class="global-navigation">
+      <li><a href="<?php echo get_home_url(); ?>">PROFILE</a></li>
+      <li><a href="<?php echo get_home_url(); ?>">SERVICE</a></li>
+      <li><a href="<?php echo get_home_url(); ?>">WORKS</a></li>
+      <li><a href="<?php echo get_home_url(); ?>">GALLERY</a></li>
+      <li><a href="<?php echo get_home_url(); ?>">BLOG</a></li>
+      <li><a href="<?php echo get_home_url(); ?>">ACCESS</a></li>
+      <li><a href="<?php echo get_home_url(); ?>">CONTACT</a></li>
+    </ul>
+  </nav>
+</header>
 <section class="slider">
   <div class="flexslider">
     <ul class="slides">
@@ -9,12 +23,19 @@
   </div>
 </section>
 <section class="welcome">
-  <div class="container">
-    <div class="component title">
-      <h1>Welcome</h1>
-      <hr>
-      <p>SECRET GIRLS GARDEN へお越し頂きまして有難うございます。 新着作品や、イベントなど各種サービスのご案内を随時更新しております。</p>
-    </div>
+  <div class="title">
+    <div class="sign"><img src="<?php echo get_template_directory_uri(); ?>/images/sign_1.png" width="105" height="41" /></div>
+    <h1>ASUMI ENDO PHOTOGRAPHY</h1>
+  </div>
+  <div class="read">
+    <h2>一期一会が生むドラマ</h2>
+    <p class="body">写真は一瞬を切り取るものでありながら、その1枚の中には<br>
+    ドラマやストーリーがある。<br>
+    撮り手の意図だけに留まらず、人や物、場所の思いやエネルギーが乗っかって<br>
+    一枚の写真は生まれていく。</p>
+  </div>
+  <div class="comp-button">
+    <a href="<?php echo get_home_url(); ?>">ABOUT</a>
   </div>
 </section>
 <?php get_footer(); ?>
