854636131dd0a59f5abbf5ec4481853500ee537d
Terashima <yamanogomora@gmail.com>
2019-03-10 23:37:04 +0900

    ミクスフォトセッションページのコーディング

---
+++ page-photosession.php
@@ -0,0 +1,121 @@
+<?php
+/*
+Template Name: ミクスフォトセッションのテンプレート
+*/
+?>
+<?php remove_filter('the_content', 'wpautop'); ?>
+<?php get_header(); ?>
+<div class="photosession">
+<section class="title">
+  <h1><span>ASUMI ENDO</span><br>PHOTO SESSION</h1>
+  <div><img src="<?php echo get_template_directory_uri(); ?>/images/photosession_top.jpg" class="responsive-img"></div>
+</section>
+<section class="information">
+  <ul class="ff-min">
+    <li>2月8日（金）</li>
+    <li>20:00〜</li>
+    <li>予約開始</li>
+  </ul>
+</section>
+<section class="conversion">
+  <button>ご予約はこちらから</button>
+</section>
+<section class="concept">
+  <div class="image"><img src="<?php echo get_template_directory_uri(); ?>/images/photosession_concept_1.jpg" class="responsive-img"></div>
+  <ul class="read ff-min">
+    <li>「可能性を可視化して、未来を先撮り」と聞くと、皆さんはどんなイメージを持ちますか？</li>
+    <li>一人一人の可能性は、目に見えない性質的なものもあれば、生まれてから備わっている顔立ち、選ぶ服装、仕草まで、様々です。</li>
+    <li>「もっとこうなりたい」</li>
+    <li>それは、誰もが持っている願望ですが大体は、自分は〇〇だから、〇〇だろう。と自分のポテンシャルを自分で決めてしまう事がとても多いのも事実です。</li>
+    <li>だけど、それって本当に本当？</li>
+    <li>遠藤あすみフォトセッションでは、ヘアメイクからライティング、撮影まで全て一人一人に合わせたオーダーメイドの撮影を行なっていきます。</li>
+    <li>素材の声を聞き、可能性を切り開く事が私の特技です。自分で「大好き！」と心から思える写真を手にして、可能性の扉を開きましょう！</li>
+  </ul>
+  <div class="image"><img src="<?php echo get_template_directory_uri(); ?>/images/photosession_concept_2.jpg" class="responsive-img"></div>
+</section>
+<section class="solusion">
+  <h1 class="ff-min">写真のお悩み<br><span>TOP5</span></h1>
+  <ul class="problem">
+    <li><i class="far fa-check-square"></i>写真が苦手で撮られる事が怖い</li>
+    <li><i class="far fa-check-square"></i>今までに気に入った写真がない</li>
+    <li><i class="far fa-check-square"></i>なりたい自分が分からない</li>
+    <li><i class="far fa-check-square"></i>コンプレックスが気になる</li>
+    <li><i class="far fa-check-square"></i>似合うポージングやメイクが分からない</li>
+  </ul>
+  <div class="answer">
+    <p class="head"><mark>どの悩みも全て問題ありません！</mark></p>
+    <p>不安は、ただ不慣れなだけ。なりたい自分は、自分だけで悩まない。コンプレックスは進化への道筋。分からないは、より良い方法に出会っていないだけ！悩みや不安をそのままにしといては、勿体無い！今よりも更なる高みを目指して進化してしまいましょう！</p>
+  </div>
+  <div class="answer2 clearfix">
+    <div class="image"><img src="<?php echo get_template_directory_uri(); ?>/images/photosession_answer_1.jpg" class="responsive-img"></div>
+    <ul class="read">
+      <li>写真には、正解がありません。<br>これをしたから「正解」というマニュアルも無ければ<br>万人に通用するノウハウなんてものもありません。</li>
+      <li>それじゃあ何が大切なの？</li>
+      <li>私は、何を「信じる」かを明確にする事が一番大切な事だと思います。〇〇したいという欲求や、こうだ！思う直感、〇〇を伝えたいという想い。</li>
+      <li>そして、互いに「信じる」事が何より大切だと思っています。</li>
+      <li>私の技術は、誰に習ったものでもなく、今までに出会ってきた1万人以上の方々と共に育ててきた「信じる力」の塊です！</li>
+      <li>些細な事でも大丈夫。もしも自分が信じれなかったら、私を信じてください！（笑）</li>
+      <li>そして、撮りたい！と思ったその直感を大切にして下さい。</li>
+      <li>皆様にお会い出来るのを楽しみにお待ちしております。</li>
+    </ul>
+  </div>
+</section>
+<section class="detail">
+  <div class="content">
+    <h1>ASUMI ENDO PHOTO SESSHION</h1>
+    <div class="block">
+      <p class="head">撮影詳細</p>
+      <ul>
+        <li>ヘアセット＆メイク込み</li>
+        <li>衣装２着まで<br>（お持ち込みお願い致します！悩んでしまったら多めに持ってきて一緒に選ぶ事も可能です。アクセサリー類も忘れずにお持ち込み下さい。）</li>
+        <li>所要時間2.5h</li>
+        <li>商用可能</li>
+        <li>撮影全データーをCDRに焼いて当日にお渡しします。（macの方はUSBをご持参ください）</li>
+      </ul>
+    </div>
+    <div class="block">
+      <p class="head">撮影料金</p>
+      <ol>
+        <li>税込4万円</li>
+        <li><i class="fas fa-asterisk"></i>当日現金でのご清算をお願い申し上げます。</li>
+        <li><i class="fas fa-asterisk"></i>キャンセルご変更は２日前まで可能です。<br>（それ以降はいかなる場合も全額ご負担となってしまいますので、予めご了承下さい。）</li>
+      </ol>
+    </div>
+    <div class="block">
+      <p class="head">その他</p>
+      <ol>
+        <li>お写真は、普段私がFBにアップしているような、バストアップから全身まで 一人ずつオーダーメイドのメイク&ライティングで撮影させて頂きます。<br>１着目、２着目と雰囲気を変えてお撮りしていきますので、 是非、雰囲気の異なるお洋服をお持ちになっていらして下さい。</li>
+        <li>小物類のお持ち込みも可能ですので、PC等のお仕事アイテム等、ご自由にご持参ください。<br>髪の毛アップを希望する方は、ヘアアクセサリー等もお忘れずに！</li>
+        <li>撮影は300枚程撮影致しまして、 その中から6枚ほど 私が目のあったお写真にレタッチをお入れして
+撮影データー＆レタッチデーターをお渡ししております。 それぞれの衣装1枚ずつ文字入れもしていますので、是非色々なシーンでお使いになってください。</li>
+        <li>当日はスッピン又はベースメイク程度のナチュラルなお化粧でお越しくださいね！</li>
+      </ol>
+    </div>
+    <div class="block">
+      <p class="head">撮影場所</p>
+      <ol>
+        <li>STUDIOKUMU</li>
+      </ol>
+    </div>
+    <div class="block">
+      <p class="head">住所</p>
+      <ol>
+        <li>東京都渋谷区広尾1-4-10 コウキビル2F<br>（恵比寿駅徒歩10分 スタジオは明治通り沿いで和食のお店「イワカムツカリ」さんの隣のレンガのビルの２Fになります。）</li>
+        <li><a href="#">道に迷ってしまいそうな方はこちら</a></li>
+      </ol>
+    </div>
+    <div class="block">
+      <p class="head">電話</p>
+      <ol>
+        <li>03-3442-9600（当日に道に迷われた場合はこちらにお電話下さい！）</li>
+      </ol>
+    </div>
+  </div>
+</section>
+<section class="conversion">
+  <button>ご予約はこちらから</button>
+</section>
+</div>
+<?php get_footer(); ?>
+
+
