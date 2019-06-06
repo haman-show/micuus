c523022b65106862b20aebd70ea152da101375f3
Terashima <yamanogomora@gmail.com>
2019-06-06 15:41:53 +0900

    Edit: トップページ コンセプト部分のコーディング

--- index.php
+++ index.php
@@ -1,7 +1,35 @@
 <?php get_header(); ?>
-<div class="top-page">
+<div class="content top-page">
 <section class="banner">
   <a href="/" target="_blank" class="scale-effect"><img src="<?php echo get_template_directory_uri(); ?>/images/top/banner-youtube.jpg" alt="新しい私に出会う Mr kumu 変身チャンネルスタート! YouTube" class="responsive-img"></a>
+</section>
+<section class="concept clearfix">
+  <div class="photo">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/top/concept-photo.jpg" class="responsive-img">
+  </div>
+  <div class="message" style="background-image: url(<?php echo get_template_directory_uri(); ?>/images/top/concept-background.jpg);">
+    <div class="text">
+      <div class="holder">
+        <h1 class="ff-min">CONCEPT</h1>
+        <p>
+          一人一人の中に無限に眠る可能性を<br>
+          写真と言う見える形にしてデザインし<br>
+          新しい未来を切り開く世界を提案する。
+        </p>
+        <p>
+          スタジオクムはヘアメイクから撮影まで<br>
+          一人のアーティストが創り上げ
+        </p>
+        <p>
+          表現できる世界を<br>
+          皆様にお届けしています。
+        </p>
+        <div class="comp-button">
+          <a href="/">SERVICE</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </section>
 </div>
 <?php get_footer(); ?>
