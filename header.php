46fe2457388c6af9d6e21195dcfb0b99e25f9dc8
Terashima <yamanogomora@gmail.com>
2019-06-06 14:34:45 +0900

    Edit: トップページのコーディング

--- header.php
+++ header.php
@@ -31,3 +31,21 @@
   </script>
 </head>
 <body>
+<header>
+<section class="global-header" style="background-image:url(<?php echo get_template_directory_uri(); ?>/images/global-header/background.png);">
+  <h1><img src="<?php echo get_template_directory_uri(); ?>/images/global-header/txt-title.png" width="220" height="90" alt="STUDIO KUMU" /></h1>
+  <nav>
+    <ul>
+      <li><a href="/">TOP</a></li>
+      <li><a href="/service">SERVICE</a></li>
+      <li><a href="/artist">ARTIST</a></li>
+      <li><a href="/contact">CONTACT</a></li>
+      <li><a href="/access">ACCESS</a></li>
+    </ul>
+  </nav>
+  <div class="social">
+    <a href="" class="facebook"><i class="fab fa-facebook-f fa-lg"></i></a>
+    <a href="" class="youtube"><i class="fab fa-youtube fa-lg"></i></a>
+  </div>
+</section>
+</header>
