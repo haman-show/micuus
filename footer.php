41e940e9db1dd0d20320798e24112b2b8731f9d9
Terashima <yamanogomora@gmail.com>
2019-06-07 20:10:41 +0900

    Edit: フッターのコーディング

--- footer.php
+++ footer.php
@@ -1,25 +1,44 @@
 </body>
 <footer>
-  <div class="container">
-    <div class="footer-container">
-      <div class="block">
-        <p class="head">WELCOME</p>
-        <p class="body read-text">Thank you for visiting artaito. Let me explain why I named my website 'artratio'.</p>
-      </div>
-      <div class="block">
-        <p class="head">RECENT POSTS</p>
-      </div>
-      <div class="block">
-        <p class="head">PAGES</p>
-      </div>
-      <div class="block">
-        <p class="head">INSTAGRAM</p>
-      </div>
+  <div class="footer-container">
+    <div class="box">
+      <div class="head">SERVICE</div>
+      <ul class="body">
+        <li><a href="">STANDARD</a></li>
+        <li><a href="">PREMIUM</a></li>
+        <li><a href="">ASUMI ENDO</a></li>
+      </ul>
+    </div>
+    <div class="box">
+      <div class="head">ARTIST</div>
+      <ul class="body">
+        <li><a href="">Mr kumu</a></li>
+        <li><a href="">ASUMI ENDO</a></li>
+        <li><a href="">YUMI EINO</a></li>
+      </ul>
+    </div>
+    <div class="box">
+      <div class="head">ACCESS</div>
+      <ul class="body">
+        <li><a href="">ADDRESS</a></li>
+        <li><a href="">MAP</a></li>
+      </ul>
+    </div>
+    <div class="box">
+      <div class="head">CONTACT</div>
+      <ul class="body">
+        <li><a href="">PHONE</a></li>
+        <li><a href="">MAIL</a></li>
+      </ul>
+    </div>
+  </div>
+  <div class="bottom-container clearfix">
+    <h1>Copyright STUDIOKUM All Right Reserved</h1>
+    <div class="social">
+      <a href="" class="facebook"><i class="fab fa-facebook-f fa-lg"></i></a>
+      <a href="" class="youtube"><i class="fab fa-youtube fa-lg"></i></a>
     </div>
   </div>
 </footer>
-<section class="copyright">
-  <h1>Copyright STUDIO KUM All Right Reserved</h1>
-</section>
 <script src="<?php echo get_template_directory_uri(); ?>/script.js"></script>
 </html>
