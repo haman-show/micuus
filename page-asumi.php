07a31862f446d2db10dbfa3839f43468a6ac87da
Terashima <yamanogomora@gmail.com>
2019-06-13 19:03:18 +0900

    Edit: アスミアーティストページのコーディングA

--- page-asumi.php
+++ page-asumi.php
@@ -4,8 +4,10 @@ Template Name: アスミ アーティストページのテンプレート
 */
 ?>
 <?php get_header(); ?>
-<?php get_template_part('inc/globalheader'); ?>
-<div class="contents creator">
+<div class="contents asumi">
+<section class="logo">
+  <h1><a href="<?php echo home_url('/'); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/common/logo.png" alt="STUDIO KUMU" class="responsive-img" /></a></h1>
+</section>
 <section class="slider">
   <div class="flexslider">
     <ul class="slides">
@@ -15,119 +17,25 @@ Template Name: アスミ アーティストページのテンプレート
     </ul>
   </div>
 </section>
-<section class="content comp-photo studio-photo">
-  <div class="pic"><img src="<?php echo get_template_directory_uri(); ?>/images/asumi/pic_studio_1.jpg" class="responsive-img"></div>
-  <div class="pic"><img src="<?php echo get_template_directory_uri(); ?>/images/asumi/pic_studio_2.jpg" class="responsive-img"></div>
-  <div class="txt-section-read ff-min">素直に生きると心地良い。</div>
-  <div class="txt-section-title">STUDIO<br>PORT<br>LAITE</div>
-  <div class="txt-box">
-    <p class="head">可能性を可視化して<br>未来を先撮りしよう</p>
-    <p class="read">自分の可能性は自分には見え辛い。<br>「今の自分が全て」だって<br>そんな事はなくて</p>
-    <p class="read">いつだってやってみたい！<br>と思った時がタイミング</p>
-    <p class="comp-button">
-      <a href="<?php echo home_url('/photosession'); ?>">SEE MORE</a>
-    </p>
+<section class="consept">
+  <div class="sign">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/asumi/text_sign.png" width="985" height="230">
   </div>
-</section>
-<section class="content profile">
-  <div class="pic"><img src="<?php echo get_template_directory_uri(); ?>/images/asumi/pic_profile_1.jpg" class="responsive-img"></div>
-  <div class="txt-box">
-    <p class="name">-ASUMI ENDO 遠藤あすみ-</p>
-    <p class="head">
-      <span>HAIR & MAKE</span>
-      <span>&times;</span>
-      <span>PHOTO &times; MIND</span>
-    </p>
-    <p class="read">
-      <span>ヘアメイクから撮影まで、一人で手掛けるから生まれる</span>
-      <span>マッチングの良さが特徴。</span>
-    </p>
-    <p class="read">
-      <span>「可能性を可視化して、未来を先撮り」をコンセプトに</span>
-      <span>STUDIO KUMU アーティストとして、10000人を超える</span>
-      <span>撮影経験を持ち、人物を中心としたジャンルの問わない撮影を</span>
-      <span>行なっている。</span>
-    </p>
-    <p class="read">
-      <span>趣味は洋裁とDIY！</span>
-      <span>創るとは何か、クリエイティブとは何かを追求する事が好きで</span>
-      <span>主催するレッスンでは、技術だけでなく、クリエイティブの本質に</span>
-      <span>触れる、ワークショップ等も取り入れ、</span>
-      <span>創作人間を増やす活動に力を入れている。</span>
-    </p>
-  </div>
-</section>
-<section class="content comp-photo location-photo">
-  <div class="pic"><img src="<?php echo get_template_directory_uri(); ?>/images/asumi/pic_location_1.jpg" class="responsive-img"></div>
-  <div class="txt-section-read ff-min">人の持つ奥行きに触れたい</div>
-  <div class="txt-box">
-    <p class="head">LOCATION<br>PHOTO</p>
-    <p class="read">出会いは人のみならず<br>
-ふらっと訪れた場所や<br>
-ふと手に取った物であったり様々だ。</p>
-    <p class="read">人の持つ奥行きを探る<br>
-ロケ撮影。</p>
-    <p class="comp-button">
-      <a href="<?php echo home_url('/photosession'); ?>">SEE MORE</a>
-    </p>
-  </div>
-</section>
-<section class="content pick-up-column">
-  <h1>PICK UP COLUMN</h1>
-  <h2>撮影コラムやクリエイティブのお話し、イベントレポなどをまとめています！</h2>
-  <ul class="list-holder clearfix">
-    <li>
-      <div class="pic">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/asumi/column_1.jpg" class="responsive-img">
-      </div>
-      <div class="text">
-        <div class="title">
-          <p>BABY CALDAMON</p>
-        </div>
-        <div class="read">サイトオープンに伴いメインヴィジュアルの撮影を行いました！</div>
-      </div>
-    </li>
-    <li>
-      <div class="pic">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/asumi/column_2.jpg" class="responsive-img">
-      </div>
-      <div class="text">
-        <div class="title">
-          <p>IDEA DESIGN × VENI aja</p>
-        </div>
-        <div class="read">デザイン事務所イデアデザイン様のご依頼で、新規オープンのヘアサロンの内装撮影へ！</div>
-      </div>
-    </li>
-    <li>
-      <div class="pic">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/asumi/column_3.jpg" class="responsive-img">
-      </div>
-      <div class="text">
-        <div class="title">
-          <p>BABY CALDAMON</p>
-        </div>
-        <div class="read">サイトオープンに伴いメインヴィジュアルの撮影を行いました！</div>
-      </div>
-    </li>
-    <li>
-      <div class="pic">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/asumi/column_4.jpg" class="responsive-img">
-      </div>
-      <div class="text">
-        <div class="title">
-          <p>IDEA DESIGN × VENI aja</p>
-        </div>
-        <div class="read">デザイン事務所イデアデザイン様のご依頼で、新規オープンのヘアサロンの内装撮影へ！</div>
-      </div>
-    </li>
+  <h2 class="ff-min">可能性を可視化して未来を先撮りする</h2>
+  <h1>ASUI ENDO PHOTOGRAPHY</h1>
+  <ul>
+    <li>私を創っているのは、私だけじゃない。</li>
+    <li>髪や骨格や、纏ったお洋服だけでもない。<br>
+    今までに出会った人や、悩んだ事、決めた事そうした全ての選択が<br>
+    今の私を創ってる</li>
+    <li>今まで歩んできた貴方の道を視て、貴方を創っている全ての素材の声を<br>
+    写真として可視化します。</li>
+    <li>自分の可能性は自分で決めたらいけない。<br>
+    気付いている魅力も、まだ気付いてない魅力も、
+    もっと引き出そう！</li>
   </ul>
-  <div class="comp-button">
-    <a href="/">SEE MORE</a>
-  </div>
 </section>
-<section class="content works">
-  <h1>For BOOK</h1>
-  <h2>書籍の帯撮影はこちら</h2>
+<section class="works">
   <ul class="cover-list clearfix">
     <li>
       <img src="<?php echo get_template_directory_uri(); ?>/images/asumi/cover_1.jpg" class="responsive-img">
