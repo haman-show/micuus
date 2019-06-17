d147cf0c4661c4717b1cb5bdf0f2fbcfd922865a
Terashima <yamanogomora@gmail.com>
2019-06-17 13:56:46 +0900

    スタジオポートレート コンセプト部分のコーディング

--- page-studioportrait.php
+++ page-studioportrait.php
@@ -31,5 +31,27 @@ Template Name: スタジオポートレートのテンプレート
     </div>
   <div>
 </section>
+<section class="concept">
+  <div class="photo"><img src="<?php echo get_template_directory_uri(); ?>/images/studioportrait/concept_photo_a.jpg" class="responsive-img"></div>
+  <div class="holder">
+    <h1 class="ff-min">自分のポテンシャルは自分で決めちゃいけない！</h1>
+    <h2>The only one photography</h2>
+    <ul>
+      <li>一人一人の可能性は、目に見えない性質的なものもあれば、<br>
+生まれてから備わっている顔立ち、選ぶ服装、仕草まで、様々です。</li>
+      <li>「もっとこうなりたい」</li>
+      <li>それは、誰もが持っている願望ですが<br>
+      大体は、自分は〇〇だから、〇〇だろう。と<br>
+      自分のポテンシャルを自分で決めてしまう事がとても多いのも事実です。</li>
+      <li>だけど、それって本当に本当？</li>
+      <li>遠藤あすみフォトセッションでは、<br>
+      ヘアメイクからライティング、撮影まで全て一人一人に合わせた<br>
+      オーダーメイドの撮影を行なっていきます。</li>
+      <li>素材の声を聞き、可能性を切り開く事が私の特技です。<br>
+      自分で「大好き！」と心から思える写真を手にして、可能性の扉を開きましょう！</li>
+    </ul>
+  </div>
+  <div class="photo"><img src="<?php echo get_template_directory_uri(); ?>/images/studioportrait/concept_photo_b.jpg" class="responsive-img"></div>
+</section>
 </div>
 <?php get_template_part('inc/asumi-footer'); ?>
